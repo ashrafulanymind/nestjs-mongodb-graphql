874c3f4dda9d9b33b1f8a5501dbcce99aab6ae41
Ashraful Islam <ashraful@anymindgroup.com>
2021-03-09 02:20:09 +0700

    Association

--- src/lesson/lesson.entity.ts
+++ src/lesson/lesson.entity.ts
@@ -1,3 +1,4 @@
+import { StudentType } from '../student/student.type';
 import { Column, Entity, PrimaryColumn, ObjectIdColumn } from 'typeorm';
 
 @Entity()
@@ -16,4 +17,7 @@ export class Lesson {
 
   @Column()
   endDate: string;
+
+  @Column()
+  students: string[];
 }
