3e10c3ce3460c0f562614a0f1f697a1bcc1e8c52
Ashraful Islam <ashraful@anymindgroup.com>
2021-03-09 16:41:56 +0700

    Use env

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,7 @@ import { StudentModule } from './student/student.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url:
-        'mongodb+srv://testuser:testuser@cluster0.aums2.mongodb.net/nestjs-graphql?retryWrites=true&w=majority',
+      url: `mongodb+srv://${process.env.HEROKU_MONGO_USERNAME}:${process.env.HEROKU_MONGO_PASSWORD}@cluster0.aums2.mongodb.net/nestjs-graphql?retryWrites=true&w=majority`,
       synchronize: true,
       useUnifiedTopology: true,
       entities: [Lesson, Student],
